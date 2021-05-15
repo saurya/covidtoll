9d8bd77f4fb42830076e25fd8466b08152b721c2
Saurya Velagapudi <saurya@gmail.com>
2021-05-15 15:45:47 +0800

    Add form tailwind plugin and vue file

--- src/App.vue
+++ src/App.vue
@@ -94,6 +94,7 @@
           <div class="px-4 py-8 sm:px-0">
             <div class="border-4 border-dashed border-gray-200 rounded-lg h-96" />
           </div>
+          <feed></feed>
           <!-- /End replace -->
         </div>
       </main>
